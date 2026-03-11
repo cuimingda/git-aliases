d46b92d6f1da2530be4cfb23fd5410ec814a5620
cuimingda <cuimingda@gmail.com>
2026-03-11 15:54:45 +0800

    add init.sh

---
+++ init.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+set -e
+
+ensure_git_alias() {
+  local name=$1
+  local value=$2
+
+  current=$(git config --global "alias.$name" || true)
+
+  if [ "$current" != "$value" ]; then
+    git config --global "alias.$name" "$value"
+    echo "configured git alias: $name"
+  fi
+}
+
+ensure_git_alias cb  "rev-parse --abbrev-ref HEAD"
+
